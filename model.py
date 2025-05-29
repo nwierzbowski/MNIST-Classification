01e23a7d5a3a03612894165de064915e1560c12a
Nick Wierzbowski <nwierzbow@gmail.com>
2025-05-29 11:21:09 -0400

    refactor: move model definition to separate file

---
+++ model.py
@@ -0,0 +1,40 @@
+import torch.nn as nn
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2, padding=1)
+        self.bn3 = nn.BatchNorm2d(32)
+
+        self.conv4 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.bn4 = nn.BatchNorm2d(64)
+        self.conv5 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.bn5 = nn.BatchNorm2d(64)
+        self.conv6 = nn.Conv2d(64, 64, kernel_size=5, stride=2, padding=1)
+        self.bn6 = nn.BatchNorm2d(64)
+
+        self.conv7 = nn.Conv2d(64, 128, kernel_size=4, stride=1, padding=1)
+        self.bn7 = nn.BatchNorm2d(128)
+
+        self.fc1 = nn.Linear(128 * 5 * 5, 10)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu(self.bn1(self.conv1(x)))  # Apply first convolution and ReLU
+        x = self.relu(self.bn2(self.conv2(x)))
+        x = self.relu(self.bn3(self.conv3(x)))
+
+        x = self.relu(self.bn4(self.conv4(x)))  # Apply second convolution and ReLU
+        x = self.relu(self.bn5(self.conv5(x)))
+        x = self.relu(self.bn6(self.conv6(x)))
+
+        x = self.relu(self.bn7(self.conv7(x)))  # Apply third convolution and ReLU
+        
+        x = x.view(x.size(0), -1)
+        x = torch.log_softmax(self.fc1(x), dim=1)
+
+        return x
--- train.py
+++ train.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 import torch.optim as optim
 from torchvision import datasets, transforms
 
+# Import my CNN model
+from model import CNN
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
@@ -30,44 +33,7 @@ train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=128, shuffl
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=128, shuffle=False)
 
 
-class CNN(nn.Module):
-    def __init__(self):
-        super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(32)
-        self.conv2 = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(32)
-        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2, padding=1)
-        self.bn3 = nn.BatchNorm2d(32)
 
-        self.conv4 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-        self.bn4 = nn.BatchNorm2d(64)
-        self.conv5 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
-        self.bn5 = nn.BatchNorm2d(64)
-        self.conv6 = nn.Conv2d(64, 64, kernel_size=5, stride=2, padding=1)
-        self.bn6 = nn.BatchNorm2d(64)
-
-        self.conv7 = nn.Conv2d(64, 128, kernel_size=4, stride=1, padding=1)
-        self.bn7 = nn.BatchNorm2d(128)
-
-        self.fc1 = nn.Linear(128 * 5 * 5, 10)
-        self.relu = nn.ReLU()
-
-    def forward(self, x):
-        x = self.relu(self.bn1(self.conv1(x)))  # Apply first convolution and ReLU
-        x = self.relu(self.bn2(self.conv2(x)))
-        x = self.relu(self.bn3(self.conv3(x)))
-
-        x = self.relu(self.bn4(self.conv4(x)))  # Apply second convolution and ReLU
-        x = self.relu(self.bn5(self.conv5(x)))
-        x = self.relu(self.bn6(self.conv6(x)))
-
-        x = self.relu(self.bn7(self.conv7(x)))  # Apply third convolution and ReLU
-        
-        x = x.view(x.size(0), -1)
-        x = torch.log_softmax(self.fc1(x), dim=1)
-
-        return x
 
 
 model = CNN().to(device)  # Move model to the appropriate device
